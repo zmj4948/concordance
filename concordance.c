f23460a6ce51d9f5d5949429801426a80b1a3ff3
Zoe M James <zmj4948@cs.rit.edu>
2018-10-16 14:20:04 -0400

    stubbed out the traverse_tree(), cleanup_tree(), and insert_word() functions.

--- concordance.c
+++ concordance.c
@@ -8,3 +8,20 @@ typedef struct TreeNode_st{
 	struct TreeNode_st *left;   //node's left child
 	struct TreeNode_st *right;  //node's right child
 }TreeNode;
+
+void insert_word(TreeNode **root, const char *word){
+	//pointer to the variable that contians the pointer 
+	//to the root node of the tree
+	//point to the NUL-terminated array of character containing
+	//a word that is to be inserted into the tree
+}
+
+void traverse_tree(const TreeNode *root){
+	//pointer to the first node in a tree 
+}
+
+void cleanup_tree(const TreeNode *root){
+	//pointer to the root node of a tree which is to be 
+	//deallocated. If this is null, the tree is empty
+
+}
