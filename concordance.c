9093a4882e2910369658cf51c12a2de940518871
Zoe M James <zmj4948@cs.rit.edu>
2018-10-18 17:07:50 -0400

    verified that my insert/ travesal functions worked well. They do.

--- concordance.c
+++ concordance.c
@@ -1,4 +1,8 @@
-#include "tree.h"
+//#include "tree.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int CurrentLine=0;
 
 typedef struct TreeNode_st{
 	char *word;                 //word held in this node
@@ -9,19 +13,85 @@ typedef struct TreeNode_st{
 	struct TreeNode_st *right;  //node's right child
 }TreeNode;
 
+TreeNode *newNode(char *word){
+        //hi i'm a helper function
+        TreeNode *temp=(TreeNode *)malloc(sizeof(TreeNode));
+        char *node_word=(char *)malloc(sizeof(char)*strlen(word));
+        strcpy(node_word,word);
+        temp->word=node_word;
+        temp->left=temp->right=NULL;
+        temp->frequency=1;
+        temp->refLength=3;
+        temp->references=malloc(10*sizeof(int));
+        temp->references[0]=CurrentLine;
+        return temp;
+}
+
 void insert_word(TreeNode **root, const char *word){
 	//pointer to the variable that contians the pointer 
 	//to the root node of the tree
 	//point to the NUL-terminated array of character containing
-	//a word that is to be inserted into the tree
+	//a word that is to be inserted into the tree 
+	if(*root==NULL){
+		//just insert here
+		char *pleaseWork=(char *)word;
+		*root= newNode(pleaseWork);
+	}else{
+		if(strcasecmp(word,(*root)->word)<0){
+			//left
+			insert_word(&((*root)->left),word);
+		}else if(strcasecmp(word,(*root)->word)>0){
+			//right
+			insert_word(&((*root)->right),word);
+		}else{
+			//WE FOUND A GOD-DAMN MATCH 
+			if((*root)->frequency<(*root)->refLength){
+				(*root)->frequency+=1;
+				(*root)->references[((*root)->frequency)-1]=CurrentLine;		
+			}else{
+				(*root)->references=realloc((*root)->references,(3*sizeof(int))+3);
+				(*root)->refLength+=3;
+				(*root)->references[((*root)->frequency)-1]=CurrentLine;
+			}
+		
+		}
+	}	
 }
 
 void traverse_tree(const TreeNode *root){
-	//pointer to the first node in a tree 
+	//pointer to the first node in a tree
+	if(root==NULL)
+		return;
+	traverse_tree(root->left);
+	printf("%s : %d\n",root->word, root->frequency);
+	traverse_tree(root->right); 
 }
 
 void cleanup_tree(const TreeNode *root){
 	//pointer to the root node of a tree which is to be 
 	//deallocated. If this is null, the tree is empty
+	if(root!=NULL){
+		cleanup_tree(root->left);
+		cleanup_tree(root->right);
+		free(root->word);
+		free(root->references);
+		free(&root);
+	}
+}
 
+int main(int argc,char *argv[]){
+	if(argc>1){
+		fprintf(stderr,"useage: concordance\n");
+	}else{
+		//TO-DO
+		//when calling insert, pass the address of the node into it
+		TreeNode *root=NULL;
+		insert_word(&root,"BITCH");
+		insert_word(&root,"fuck");
+		insert_word(&root,"help");
+		insert_word(&root,"me");
+		traverse_tree(root);
+		
+	}
+	return 0;
 }
